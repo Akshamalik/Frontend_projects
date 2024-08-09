ff53ef7a818aae5406289615bc906b36f4e878ad
Akshamalik <aksha5u786@gmail.com>
2024-08-09 13:03:00 +0530

    tooGoodCo webiste started

---
+++ tooGoodco/js/script.js
@@ -0,0 +1,55 @@
+// locomotive code
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+// to make the play button
+function videoconAnimation() {
+  var videocon = document.querySelector("#video-container");
+  var playbtn = document.querySelector("#play");
+
+  videocon.addEventListener("mouseenter", function () {
+    //alert("HIII")
+    gsap.to(playbtn, {
+      scale: 1,
+      opacity: 1,
+    });
+  });
+
+  videocon.addEventListener("mouseleave", function () {
+    gsap.to(playbtn, {
+      opacity: 0,
+      scale: 0,
+    });
+  });
+
+  videocon.addEventListener("mousemove", function (dets) {
+    gsap.to(playbtn, {
+      left: dets.x - 50,
+      top: dets.y - 80,
+    });
+  });
+}
+
+videoconAnimation();
+
+
+// function to load the h1 animation
+function loadingAnimation(){
+    gsap.from("#page1 h1",{
+        y:100,
+        opacity:0,
+        delay:0.5,
+        duration:0.9,
+        stagger:0.2
+    }) 
+    gsap.from("#page1 #video-container",{
+        scale:0.9,
+        opacity:0,
+        delay:1.3,
+        duration:0.5
+    })  
+}
+
+loadingAnimation();
